2b2f970cfca7e678d46c45fe496e40af614f6ee1
ataide79 <106406979+ataide79@users.noreply.github.com>
2022-10-10 16:38:37 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+menuListArray = ["Pizza de calabresa","pizza 4 queijos","pizza Marguerita ","pizza Muçarela","pizza Creme de avelã com frutas"//adicionar mais itens
+                    ];
+
+function addItem(){
+var htmldata;
+var img="<img id='im1' src='images/pizzaImg.png'>";
+var item=document.getElementById("addItem").value;
+menuListArray.sort();
+htmldata=" ";
+for(var i=0;i<menuListArray.length;i++){
+    htmldata=htmldata+img+menuListArray[i]+"<br>";
+
+    
+}
+document.getElementById("displayAddedMenu").innerHTML=htmldata;
+
+//Completar o código
+}
+
+function getMenu(){
+    var htmldata;
+    
+   
+    menuListArray.sort();
+    htmldata=" ";
+    for(var i=0;i<menuListArray.length;i++){
+        htmldata=htmldata+menuListArray[i]+"<br>";
+    
+        
+    }
+    document.getElementById("displayMenu").innerHTML=htmldata;
+
+}
+
+function addTop(){
+    var item=document.getElementById("addItem").value;
+    menuListArray.push(item);
+    addItem();
+//Completar o código
+}
